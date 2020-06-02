f69b890b15a9e2962aa95b9acdcf01aa3142e77b
J.Goel <jgoel15@outlook.com>
2020-06-02 14:14:27 +0530

    Http client added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<any>('https://localhost:5001/api/products?pageSize=50')
+      .subscribe(
+        (response: IPagination) => {
+          this.products = response.data;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 }
